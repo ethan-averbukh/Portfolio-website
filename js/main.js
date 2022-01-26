13d3ded7f4a499aa9b27271fa0ccda140e311e69
Ethan Averbukh <averbukh.ethan@gmail.com>
2022-01-25 18:15:00 -0700

    Added click functionality to hamburger menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(()=> {
-    console.log("hello");
+    let $hmbrgrIcon = $('.hmbrgr-menu');
+    let $dropDownMenu = $('div.menu-container');
+    let $body = $('body');
+
+    $hmbrgrIcon.on('click', function() {
+        $dropDownMenu.toggleClass("dropdown-content");
+    });
+ 
 })
